69162072ab1391f54357de354ed7630395744105
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-07-15 15:35:34 +0530

    Code Coverage

--- src/main/java/DemoCode/DemoClass.java
+++ src/main/java/DemoCode/DemoClass.java
@@ -6,7 +6,7 @@ public class DemoClass {
 	private String name;
 	private int rollno;
 
-	public void Q1_Student(String s, int i) {
+	public void SetDeta(String s, int i) {
 		this.name = s;
 		this.rollno = i;
 	}
@@ -15,8 +15,6 @@ public class DemoClass {
 		System.out.println("name is   " + name + "   rollno is   " + rollno);
 	}
 
-	public static void main(String[] args) {
-		DemoClass stud = new DemoClass();
-		stud.display();
+	
 	}
-}
+
---
+++ src/test/java/App_Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import DemoCode.DemoClass;
+
+public class App_Test {
+
+	@Test
+	public void e1() {
+		DemoClass obj = new DemoClass();
+		 obj.display();
+	}
+}
