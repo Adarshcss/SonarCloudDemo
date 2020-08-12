4d4bcf13cf45e105f194e09e6306d2659188411b
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-12 12:45:06 +0530

    Push SonarServer

---
+++ Sonar Execute.bat
@@ -0,0 +1,1 @@
+mvn install & mvn verify sonar:sonar
--- src/test/java/App_Test.java
+++ src/test/java/App_Test.java
@@ -7,7 +7,9 @@ public class App_Test {
 	@Test
 	public void e1() {
 		DemoClass obj = new DemoClass();
-		 obj.display();
-		 obj.SetDeta(null, 0);
+		 //obj.display();
+		 //obj.SetDeta(null, 0);
+		 obj.display_ssr();
+		 obj.display_ssr1();
 	}
 }
